604613be4043c006688ed82d60b9a94365584227
RaresDrg <dragan_rares13@yahoo.com>
2024-01-10 05:12:22 +0200

    javascript-lesson-19-module-10
    javascript-lesson-19-module-10

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
 const ENDPOINT = 'https://newsapi.org/v2/everything';
-const KEY_API = 'c16730bd019e47b49168e3f2670f55aa';
+const KEY_API = 'c16730bd019e47b49168e3f2670f55aa' ;
 
 function getNews(query) {
   const url = `${ENDPOINT}?apiKey=${KEY_API}&q=${query}`;
